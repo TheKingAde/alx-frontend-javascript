0da7f9fac695c769afa38e3e7517bb8f9c16d17f
Ade <theking-ade@outlook.com>
2024-04-25 04:52:55 +0000

    uppdate file 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,23 @@
 // 3-all.js
 
-export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+// 3-all.js
+
+import { uploadPhoto, createUser } from './utils';
+
+async function handleProfileSignup() {
+  try {
+    // Use Promise.all to concurrently execute uploadPhoto and createUser functions
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    // Log the combined result to the console
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    // Log an error message if any of the promises reject
+    console.error('Signup system offline');
+  }
 }
+
+export default handleProfileSignup;

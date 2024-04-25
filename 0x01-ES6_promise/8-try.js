841fde077446dd8e2e3efd17cc8de0ec6a2d7e22
Ade <theking-ade@outlook.com>
2024-04-25 04:45:03 +0000

    add files (2-9).js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,6 @@
+// 8-try.js
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload])
+    .then((value) => value);
+}
---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+// 9-try.js
+
+export default function mathFunction(guardrail) {
+  const queue = [];
+  try {
+    const x = guardrail();
+    queue.push(x);
+  } catch (err) {
+    queue.push(`${err.name}: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}

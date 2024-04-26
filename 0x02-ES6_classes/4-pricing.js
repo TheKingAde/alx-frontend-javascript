2a72406a76e2fc03fffc0f71e63d944712a19304
Ade <theking-ade@outlook.com>
2024-04-26 05:08:40 +0000

    add file 4-pricing.js and 5-building.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,37 @@
 // 4-pricing.js
 class Pricing {
   constructor(amount, currency) {
+    // eslint-disable-next-line no-underscore-dangle
     this._amount = amount;
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = currency;
   }
 
   // Getter and setter for amount
   get amount() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._amount;
   }
 
   set amount(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._amount = value;
   }
 
   // Getter and setter for currency
   get currency() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._currency;
   }
 
   set currency(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = value;
   }
 
   // Method to display full price
   displayFullPrice() {
+    // eslint-disable-next-line no-underscore-dangle
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,21 @@
 // 5-building.js
 class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
-    }
+  constructor(sqft) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._sqft = sqft;
+  }
 
-    // Getter for sqft
-    get sqft() {
-        return this._sqft;
-    }
+  // Getter for sqft
+  get sqft() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._sqft;
+  }
 
-    // Abstract method evacuationWarningMessage
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  // Abstract method evacuationWarningMessage
+  evacuationWarningMessage() {
+    this.toString();
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
 
 export default Building;

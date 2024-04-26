36e368833a46c9cb2640d0687e0bd0e08ded8170
Ade <theking-ade@outlook.com>
2024-04-26 23:24:15 +0000

    fix file 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function* createIteratorObject(report) {
-  for (const department in report.allEmployees) {
+  const departments = Object.keys(report.allEmployees);
+  for (const department of departments) {
     const employees = report.allEmployees[department];
     for (const employee of employees) {
       yield employee;

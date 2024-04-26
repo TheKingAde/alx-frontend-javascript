46467f72e8f29c2062abc7c9f5e6365ea5a44359
Ade <theking-ade@outlook.com>
2024-04-26 05:23:48 +0000

    add file 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+// 7-airport.js
+class Airport {
+  constructor(name, code) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = name;
+    // eslint-disable-next-line no-underscore-dangle
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._code;
+  }
+
+  // Default string description
+  toString() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `Airport [${this._code}]`;
+  }
+}
+
+export default Airport;

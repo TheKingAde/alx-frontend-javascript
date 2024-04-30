4ba9889b6a22168bc2d506559fc2568c2cb4be72
Ade <theking-ade@outlook.com>
2024-04-30 21:37:41 +0000

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+
+  return filteredValues.map(value => value.slice(startString.length)).join('-');
+}
